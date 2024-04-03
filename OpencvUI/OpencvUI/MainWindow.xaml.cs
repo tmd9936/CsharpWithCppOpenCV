2c4fbf5b88f9386cc73e28c8b692555d12c2da03
BaekSH <tmd9936@gmail.com>
2024-04-03 11:34:21 +0900

    UI 추가

--- OpencvUI/OpencvUI/MainWindow.xaml.cs
+++ OpencvUI/OpencvUI/MainWindow.xaml.cs
@@ -21,48 +21,16 @@ namespace OpencvUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        System.Drawing.Image img = null;
-        MemoryStream ms = null;
-        byte[] bytes = null;
+        System.Drawing.Image originImg = null;
+        System.Drawing.Image resultImg = null;
 
+        MemoryStream originMs = null;
+        MemoryStream resultMs = null;
+        byte[] bytes = null;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            try
-            {
-                ms = new MemoryStream();
-                img = System.Drawing.Image.FromFile("C:\\test\\sudoku.png");
-
-                string pf = img.PixelFormat.ToString();
-                string rf = img.RawFormat.ToString();
-
-                img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
-
-                bytes = ms.ToArray();
-                int outSize = 0;
-
-                IntPtr resultArray = CvAdaptiveThreshold(bytes, img.Width, img.Height, 255, true, true, 5, 5, out outSize);
-
-                byte[] resultBytes = new byte[outSize];
-                Marshal.Copy(resultArray, resultBytes, 0, outSize);
-
-                FreeMemory(resultArray);
-
-                ms = new MemoryStream(resultBytes);
-
-                img = System.Drawing.Image.FromStream(ms);
-
-                img.Save("C:\\test\\out111.png");
-            }
-            catch (Exception ex)
-            {
-            }
-            finally
-            {
-
-            }
         }
 
         [DllImport("CppOpenCV.dll", CallingConvention = CallingConvention.Cdecl)]
@@ -73,5 +41,93 @@ namespace OpencvUI
 
         [DllImport("CppOpenCV.dll", CallingConvention = CallingConvention.Cdecl)]
         public static extern void FreeMemory(IntPtr array);
+
+        private void ChangeImage(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                originMs = new MemoryStream();
+                originImg = System.Drawing.Image.FromFile("C:\\test\\sudoku.png");
+
+                originImg.Save(originMs, System.Drawing.Imaging.ImageFormat.Png);
+
+                BitmapImage originBitmap = new BitmapImage();
+                originBitmap.BeginInit();
+                originBitmap.StreamSource = originMs;
+                originBitmap.EndInit();
+
+                uiPicOrigin.Source = originBitmap;
+
+                bytes = originMs.ToArray();
+                int outSize = 0;
+
+                int MaxValue = (int)(MaxValueSlider.Value);
+                bool isGaussian = IsGaussianCheckBox.IsChecked.Value;
+                bool isBinary = IsBinaryCheckBox.IsChecked.Value;
+                int blockSize = (int)(BlockSizeSlider.Value);
+                int cValue = (int)(CSlider.Value);
+
+                IntPtr resultArray = CvAdaptiveThreshold(bytes, originImg.Width, originImg.Height, MaxValue, isGaussian, isBinary, blockSize, cValue, out outSize);
+
+                byte[] resultBytes = new byte[outSize];
+                Marshal.Copy(resultArray, resultBytes, 0, outSize);
+
+                FreeMemory(resultArray);
+
+                resultMs = new MemoryStream(resultBytes);
+
+                resultImg = System.Drawing.Image.FromStream(resultMs);
+
+                BitmapImage resultBitmap = new BitmapImage();
+                resultBitmap.BeginInit();
+                resultBitmap.StreamSource = resultMs;
+                resultBitmap.EndInit();
+
+                uiPicResult.Source = resultBitmap;
+
+                resultImg.Save("C:\\test\\out111.png");
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                //if (ms != null)
+                //{
+                //    ms.Dispose();
+                //    ms = null;
+                //}
+
+                //if (originImg != null)
+                //{
+                //    originImg.Dispose();
+                //    originImg = null;
+                //}
+
+                //if (resultImg != null)
+                //{
+                //    resultImg.Dispose();
+                //    resultImg = null;
+                //}
+            }
+        }
+
+        private void MaxValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (MaxValueTextBlock != null)
+                MaxValueTextBlock.Text = ((int)e.NewValue).ToString();
+        }
+
+        private void BlockSizeChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (BlockSizeTextBlock != null)
+                BlockSizeTextBlock.Text = ((int)e.NewValue).ToString();
+        }
+
+        private void CChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (CTextBlock != null)
+                CTextBlock.Text = ((int)e.NewValue).ToString();
+        }
     }
 }
